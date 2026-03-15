8228742915be731f121b10b31dcea71a7daf7e71
Leon Sorokin <leeoniya@gmail.com>
2026-03-14 19:42:21 -0500

    update ivi

--- ivi/src/main.ts
+++ ivi/src/main.ts
@@ -1,15 +1,18 @@
 import { update, component, createRoot, useState, html } from "ivi";
 
-const genData = () => Array.from({ length: 10_000 }, () => Math.random() * 100);
+function genData() {
+  return Array.from({ length: 10_000 }, () => Math.random() * 100);
+}
 
 const App = component((c) => {
   const [getData, setData] = useState<number[]>(c, genData());
+  const onclick = () => setData(genData());
 
   return () =>
     html`
       ${getData()[0]}
       <br>
-      <button @click=${() => setData(genData())}>Update data!</button>
+      <button @click=${onclick}>Update data!</button>
     `;
 });
 
